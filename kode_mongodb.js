7f8f0a002ee48a53121e6ed5b5aee9d54597bed9
NurJayantiPutriS <150574437+NurJayantiPutriS@users.noreply.github.com>
2025-05-10 23:53:55 +0800

    Add files via upload

---
+++ kode_mongodb.js
@@ -0,0 +1,6 @@
+use mahasiswa
+
+db.data.insertMany([
+  { nama: "nur jayanti putri s", nim: "D0222313", kelas: "INFORMATIKA C 2022" },
+  { nama: "Lisa", nim: "C222123", kelas: "Manajemen A 2022" },
+])
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Nama: Nur Jayanti Putri S
+NIM: D0222313
+Kelas: C 2022 Manajemen Basis Data
